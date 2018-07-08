a19561148573783b2b04f675a91180fe1716bac8
younes <youneskasri@gmail.com>
2018-07-08 15:03:53 +0100

    Remove old picture and save new one

--- routes/index.js
+++ routes/index.js
@@ -34,28 +34,49 @@ function setProfileImage(req, res, next){
 
 	console.log("setProfileImage");
 	let file = req.body.image;	
+	let idEmployee = req.body.idEmployee;
 	File.create({
 		creationDate: new Date(),
-		contentType: 'image/*',
+		contentType: 'image/png',
 		data: file
 	})
 	.then(createdFile => {
-		Employee.findByIdAndUpdate({ _id: req.body.idEmployee}, {profileImage: createdFile._id}, {new: true})
-		.populate("profileImage")
-		.exec((err, employee) => {
-			if (err) return console.log(err);
-			console.log("emp with image".green + employee)
-			let image = employee.profileImage;
-			return res.send({ success: true, image });
-		});
-	})
-	.then(()=>{
-		File.find
-	})
-	.catch(err => {
-		console.log(err.message.red);
-		fs.write('./log/'+Date.now()+'.log', err);
-	});
+		console.log(idEmployee);
+		Employee.findByIdAndUpdate({ _id: idEmployee}, {profileImage: createdFile._id}, {new: false})
+		.exec()
+		.then(deleteOldImageIfExists)
+		.then(()=>{ findEmployeeThenSendImage(res, idEmployee)})
+		.catch(printError);
+	}).catch(printError);
+}
+
+function deleteOldImageIfExists(oldEmployee){
+	if (oldEmployee.profileImage){
+		File.findByIdAndRemove({_id: oldEmployee.profileImage})
+		.exec().catch(printError);
+	} else {
+		console.log("No previous image file");
+	}	
+}
+
+function findEmployeeById(id, populateImage){
+	let query = Employee.findById({ _id: id});
+	if (populateImage) query = query.populate('profileImage');
+	return query.exec(); // promise
+}
+
+function findEmployeeThenSendImage(res, id){
+	findEmployeeById(id, true)
+	.then(employee => {
+		let image = employee.profileImage;
+		return res.send({ success: true, image });
+	}).catch(printError);
+}
+
+function printError(err){
+	console.log(err.message.red);
+	fs.write('./log/'+Date.now()+'.log', err);
+	console.log(err);
 }
 
 
