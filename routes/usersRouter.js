15198a0d57b947c3b878d20b48a4638a11989020
younes <youneskasri@gmail.com>
2018-12-08 22:07:37 +0000

    adding users CRUD

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,7 +2,9 @@ const express = require('express'),
   router = express.Router();
 
 router.get('/users', usersService.index)
+  .get('/users/new', usersService.new)
 	.post('/users', usersService.create)
+  .get('/users/:id/edit', usersService.edit)
   .get('/users/:id', usersService.show)
   .patch('/users/:id', usersService.update)
   .delete('/users/:id', usersService.remove);
