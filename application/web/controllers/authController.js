31e094fd5a07cdc1deabb5ad48498da1ca456a04
younes <youneskasri@gmail.com>
2018-12-22 00:00:15 +0100

    exports.nomFonction instead of exports {}

--- application/web/controllers/authController.js
+++ application/web/controllers/authController.js
@@ -1,28 +1,26 @@
-exports = {
-  isLoggedIn: function(req, res, next){
-      if(req.isAuthenticated()){
-          return next();
-      }
-      req.flash('error', 'MUST_BE_SIGNED_IN');
-      req.session.redirectTo = req.path;
-      res.redirect('/login');
-  },
+exports.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash('error', 'MUST_BE_SIGNED_IN');
+    req.session.redirectTo = req.path;
+    res.redirect('/login');
+};
   
-  isAdmin: function(req, res, next) {
-    if(req.user.isAdmin) {
-      next();
-    } else {
-      req.flash('error', 'MUST_BE_ADMIN');
-      res.redirect('back');
-    }
-  },
+exports.isAdmin = function(req, res, next) {
+  if(req.user.isAdmin) {
+    next();
+  } else {
+    req.flash('error', 'MUST_BE_ADMIN');
+    res.redirect('back');
+  }
+};
 
-  isSafe: function(req, res, next) {
-    if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
-      next();
-    } else {
-      req.flash('error', 'Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.');
-      res.redirect('back');
-    }
+exports.isSafe = function(req, res, next) {
+  if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
+    next();
+  } else {
+    req.flash('error', 'Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.');
+    res.redirect('back');
   }
 };
